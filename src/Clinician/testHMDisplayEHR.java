0f1fffff48221352985f2da06101c0d177a0fe26
haoming-mmu <haoming.kong9@gmail.com>
2023-09-02 17:53:07 +0800

    displaypatientEHR GUI done

---
+++ src/Clinician/testHMDisplayEHR.java
@@ -0,0 +1,8 @@
+package Clinician;
+
+public class testHMDisplayEHR {
+	public static void main(String[] args)
+	{
+		DisplayPatientEHR.createAndShowGUI(); 
+	}
+}
--- src/ehrBlockchain/app.java
+++ src/ehrBlockchain/app.java
@@ -18,15 +18,15 @@ public class app {
 			EHRchain.distribute();
 			
 		} else {	
-//			String tranx1 = "CS0001|BD/5lZqGUxOQcYEnbMrrMg==|2023-04-21";
-//			String tranx2 = "CS0002|BD/5lZqGUxOQcYEnbMrrMg==|2022-02-22";
-//			String tranx3 = "CS0002|BD/5lZqGUxOQcYEnbMrrMg==|2022-08-30";
-//			String tranx4 = "CS0001|BD/5lZqGUxOQcYEnbMrrMg==|2023-06-30";
-			
-			String tranx1 = "CS0003|BD/5lZqGUxOQcYEnbMrrMg==|2023-05-21";
-			String tranx2 = "CS0001|BD/5lZqGUxOQcYEnbMrrMg==|2022-07-22";
-			String tranx3 = "CS0003|BD/5lZqGUxOQcYEnbMrrMg==|2022-08-31";
-			String tranx4 = "CS0002|BD/5lZqGUxOQcYEnbMrrMg==|2023-09-01";
+//			String tranx1 = "CS0001|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|-|2023-04-21|-|-|-|NA";
+//			String tranx2 = "CS0002|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|-|2022-02-22|-|-|-|NA";
+//			String tranx3 = "CS0002|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|A0001|2022-08-30|-|-|-|NA";
+//			String tranx4 = "CS0001|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|A0002|2023-06-30|-|-|-|NA";
+//			
+			String tranx1 = "CS0003|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|-|2023-05-21|-|-|-|NA";
+			String tranx2 = "CS0004|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|-|2022-07-22|-|-|-|NA";
+			String tranx3 = "CS0003|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|A0003|2023-08-31|-|-|-|Healthy";
+			String tranx4 = "CS0004|BD/5lZqGUxOQcYEnbMrrMg==|-|-|-|-|-|-|-|-|-|A0004|2023-09-01|-|-|-|Death";
 			
 			RecordCollection tranxLst = new RecordCollection();
 			tranxLst.add(tranx1);
